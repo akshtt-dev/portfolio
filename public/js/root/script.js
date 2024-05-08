b577355d4026461c51877c3215e7c7c1237f49f8
akshtt-dev <maillegendop@gmail.com>
2024-05-08 14:53:48 +0530

    feat: Add dark mode and light mode functionality

--- public/js/root/script.js
+++ public/js/root/script.js
@@ -2,6 +2,7 @@ const searchInput = document.getElementById("search");
 const resetBtn = document.getElementById("reset");
 const resetSymbol = document.getElementById("resetSymbol");
 const homeAnchor = document.getElementById("home");
+const body = document.body;
 
 // Initially hide reset button and symbol
 resetBtn.style.visibility = "hidden";
@@ -34,3 +35,21 @@ const currentYear = new Date().getFullYear();
 
 // Update the content of the span element with id "currentYear"
 document.getElementById("currentYear").textContent = currentYear;
+
+// DARK MODE AND LIGHT MODE FUNCTIONALITY
+const sun = document.getElementsByClassName("light-mode")[0]; // Get the first element with class "light-mode"
+const moon = document.getElementsByClassName("dark-mode")[0]; // Get the first element with class "dark-mode"
+
+sun.addEventListener("click", () => {
+    sun.style.display = "none";
+    moon.style.display = "block";
+    body.classList.remove("dark-theme");
+    body.classList.add("light-theme");
+});
+
+moon.addEventListener("click", () => {
+    moon.style.display = "none";
+    sun.style.display = "block";
+    body.classList.remove("light-theme");
+    body.classList.add("dark-theme");
+});
