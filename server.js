d81a618aa90c10d7ea5f6cb167bbf85b952f9ba0
akshtt-dev <maillegendop@gmail.com>
2024-05-12 09:14:21 +0530

    chore: Update CSS styles and image sources

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ const staticDirectory = path.join(__dirname, 'public');
 // Middleware to serve static files
 app.use(express.static(staticDirectory));
 
+// Redirect routes with trailing slashes to routes without them
+app.use((req, res, next) => {
+  if (req.path.substr(-1) === '/' && req.path.length > 1) {
+    const query = req.url.slice(req.path.length);
+    res.redirect(301, req.path.slice(0, -1) + query);
+  } else {
+    next();
+  }
+});
+
 app.get('/', (req, res) => {
   res.header("Content-Type", "text/html");
   res.send(fs.readFileSync(root, "utf8"));
